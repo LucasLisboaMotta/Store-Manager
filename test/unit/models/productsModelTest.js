03bc0cd007f37e3a37e2895d3693c03ff2cfca8d
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-05-26 09:17:00 -0300

    criação de arquivos diversos

---
+++ test/unit/models/productsModelTest.js
@@ -0,0 +1,207 @@
+
+const sinon = require("sinon");
+const { expect } = require("chai");
+const connection = require("../../../models/concection");
+const productsModel = require("../../../models/productsModel");
+
+describe("Testando Model Sale", () => {
+
+  describe("Testando GetAll do Sales", async () => {
+    const sales = [ 
+      { id: 1, date: '2022-05-10 10:10:10' },
+      { id: 2, date: '2022-05-11 20:20:20' }
+    ];
+
+    beforeEach(async () => {
+      const execute = [sales];
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+
+    it("testando o argumento usado no connection", async () => {
+      sinon.spy(connection, "execute")
+      await productsModel.getAll();
+      const spyCall = connection.execute.getCall(0)
+      const query = 'SELECT * FROM sales'
+      expect(spyCall.args).to.equal([query]);
+    });
+
+    it("retorna um array", async () => {
+      const response = await productsModel.getAll();
+      expect(response).to.be.a("array");
+    });
+
+    it("se o array possui objetos dentro", async () => {
+      const response = await productsModel.getAll();
+      expect(response[0]).to.be.a("object");
+    });
+
+    it("se os objetos estão na forma correta", async () => {
+      const response = await productsModel.getAll();
+      expect(response[0]).to.equal({ id: 1, date: '2022-05-10 10:10:10' });
+      expect(response[1]).to.equal({ id: 2, date: '2022-05-11 20:20:20' });
+    });
+
+  });
+
+  describe("Testando GetById do Sales", async () => {
+    const sales = [ 
+      { id: 1, date: '2022-05-10 10:10:10' }
+    ];
+
+    beforeEach(async () => {
+      const execute = [sales];
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+
+    it("testando o argumento usado no conection", async () => {
+      sinon.spy(connection, "execute")
+      await productsModel.getById(1);
+      const spyCall = connection.execute.getCall(0);
+      const query = 'SELECT * FROM sales WHERE id = ?';
+      expect(spyCall.args).to.equal([query, [1]]);
+    });
+
+    it("retorna um objeto", async () => {
+      const response = await productsModel.getById(1);
+      expect(response).to.be.a("object");
+    });
+
+    it("se o objeto esta na forma correta", async () => {
+      const response = await productsModel.getById(1);
+      expect(response).to.equal({ id: 1, date: '2022-05-10 10:10:10' });
+    });
+
+  });
+
+  describe("Testando post do Sales", async () => {
+    const sales = [
+      {
+        fieldCount: 0,
+        affectedRows: 1,
+        insertId: 10,
+        info: '',
+        serverStatus: 2,
+        warningStatus: 0
+      },
+      undefined
+    ];
+
+    beforeEach(async () => {
+      const execute = sales;
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+
+    it("testando o argumento usado no conection", async () => {
+      sinon.spy(connection, "execute")
+      await productsModel.post('2022-05-10 10:10:10');
+      const spyCall = connection.execute.getCall(0)
+      const query = 'INSERT INTO sales (date) VALUES (?);'
+      expect(spyCall.args).to.equal([query, ['2022-05-10 10:10:10']]);
+    });
+
+    it("retorna um objeto", async () => {
+      const response = await productsModel.post('2022-05-10 10:10:10');
+      expect(response).to.be.a("object");
+    });
+
+    it("se o objeto possui o id correto", async () => {
+      const response = await productsModel.post('2022-05-10 10:10:10');
+      expect(response.insertId).to.equal(10);
+    });
+  });
+
+  describe("Testando Put do Sales", async () => {
+    const sales = [
+      {
+        fieldCount: 0,
+        affectedRows: 1,
+        insertId: 0,
+        info: 'Rows matched: 1  Changed: 1  Warnings: 0',
+        serverStatus: 2,
+        warningStatus: 0,
+        changedRows: 1
+      },
+      undefined
+    ];
+
+    beforeEach(async () => {
+      const execute = sales;
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+
+    it("testando o argumento usado no conection", async () => {
+      sinon.spy(connection, "execute")
+      await productsModel.put(1, '2022-05-10 20:20:20');
+      const spyCall = connection.execute.getCall(0)
+      const query = 'UPDATE sale SET date = ? WHERE id = ?;'
+      expect(spyCall.args).to.equal([query, ['2022-05-10 20:20:20', 1]]);
+    });
+
+    it("retorna um objeto", async () => {
+      const response = await productsModel.put(1, '2022-05-10 20:20:20');
+      expect(response).to.be.a("object");
+    });
+
+    it("se o objeto possui o id correto", async () => {
+      const response = await productsModel.put(1, '2022-05-10 20:20:20');
+      expect(response.affectedRows).to.equal(1);
+    });
+  });
+
+  describe("Testando Delete do Sales", async () => {
+    const sales = [
+      {
+        fieldCount: 0,
+        affectedRows: 1,
+        insertId: 0,
+        info: '',
+        serverStatus: 2,
+        warningStatus: 0
+      },
+      undefined
+    ];
+
+    beforeEach(async () => {
+      const execute = sales;
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+
+    it("testando o argumento usado no conection", async () => {
+      sinon.spy(connection, "execute")
+      await productsModel.delete(1);
+      const spyCall = connection.execute.getCall(0)
+      const query = 'DELETE FROM sales WHERE id = ?;'
+      expect(spyCall.args).to.equal([query, [1]]);
+    });
+
+    it("retorna um objeto", async () => {
+      const response = await productsModel.delete(1);
+      expect(response).to.be.a("object");
+    });
+
+    it("se o objeto possui o id correto", async () => {
+      const response = await productsModel.delete(1);
+      expect(response.affectedRows).to.equal(1);
+    });
+  });
+});
---
+++ test/unit/services/serviceTest.js
@@ -0,0 +1,15 @@
+// const sinon = require("sinon");
+
+// const jQuery = {
+//   ajax: () => {
+//     console.log('oi')
+//   }
+// }
+
+// sinon.spy(jQuery, "ajax")
+// jQuery.ajax("/stuffs", "adeus")
+// jQuery.ajax("/Segunda")
+// var spyCall = jQuery.ajax.getCall(0)
+// console.log(spyCall.args)
+
+// // assertEquals("/stuffs", spyCall.args[0])
