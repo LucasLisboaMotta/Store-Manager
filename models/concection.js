c2d22703daca08cd274605e571206f9a7a1d23bd
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-05-27 10:40:05 -0300

    removendo port do arquivo connection

--- models/concection.js
+++ models/concection.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 module.exports = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.PORT,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
