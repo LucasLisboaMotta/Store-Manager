7256f87e6ab6086eb15d3d421967c5e9b7b5c31c
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-05-27 15:53:11 -0300

    Finalizando validações das requisições nas rotas post e put do products e sales

--- middlewares/productsValidation.js
+++ middlewares/productsValidation.js
@@ -1,9 +1,8 @@
 module.exports = {
   post: (req, res, next) => {
-    console.log(req.body);
     const { name, quantity } = req.body;
     if (!name) return res.status(400).json({ message: '"name" is required' });
-    if (!quantity) return res.status(400).json({ message: '"quantity" is required' });
+    if (quantity === undefined) return res.status(400).json({ message: '"quantity" is required' });
     if (name.length < 5) { 
     return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
     } if (quantity <= 0) {
--- middlewares/salesValidation.js
+++ middlewares/salesValidation.js
@@ -1,11 +1,14 @@
+const validation = ({ quantity, productId }, res) => {
+  if (quantity === undefined) return res.status(400).json({ message: '"quantity" is required' });
+  if (!productId) return res.status(400).json({ message: '"productId" is required' });
+  if (quantity <= 0) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+};
+
 module.exports = {
   post: (req, res, next) => {
-    console.log(req.body);
-    const { quantity, productId } = req.body;
-    if (!quantity) return res.status(400).json({ message: '"quantity" is required' });
-    if (!productId) return res.status(400).json({ message: '"productId" is required' });
-    if (quantity <= 0) {
-      return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-    } return next();
+    req.body.forEach((element) => validation(element, res));
+    return next();
   },
 };
